d5f0e89385f39b0e6bf8275113ae684cf32e8649
nicola <belchuk2005@mail.ru>
2025-03-28 14:25:57 +0300

    Start file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Main file of microservice"""
 from fastapi import FastAPI
 
 from api.v1.routers import router as v1_router
@@ -7,4 +8,3 @@ Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(v1_router)
-
