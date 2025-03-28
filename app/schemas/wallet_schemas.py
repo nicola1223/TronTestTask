7c9a6626c09eb9467cf80ebb0b35be52e88e1a08
nicola <belchuk2005@mail.ru>
2025-03-28 12:50:03 +0300

    Add wallet pydantic schemas

---
+++ app/schemas/wallet_schemas.py
@@ -0,0 +1,32 @@
+"""Wallet pydantic schemas"""
+from datetime import datetime
+
+from pydantic import BaseModel
+
+
+class WalletQueryResponse(BaseModel):
+    """Model for a wallet query response"""
+    id: int
+    address: str
+    created_at: datetime
+
+    class Config:
+        """Model configuration"""
+        orm_mode = True
+
+
+class WalletInfoResponse(BaseModel):
+    """Model for a wallet information response"""
+    address: str
+    balance_trx: int
+    bandwidth: int
+    energy: int
+
+    class Config:
+        """Model configuration"""
+        orm_mode = True
+
+
+class AddressRequest(BaseModel):
+    """Model for an address request"""
+    address: str
